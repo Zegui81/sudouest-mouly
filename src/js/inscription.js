40dd5084f99170af96a9d8ee8c40936b5cfc2f82
Zegui81 <zeghmati.clement@hotmail.fr>
2018-01-18 15:08:55 +0100

    Modification contrôle inscription

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -7,15 +7,51 @@ function validateInscription() {
     var confirmPassword = formulaire["confirmPassword"].value;
     var nom = formulaire["nom"].value;
 
-    
     var listeErreur = {};
+    var ok = true;
 	// Contrôle des champs obligatoires remplis
-    if (mail === "") {
-    	$('input[name="mail"]').addClass();
+    if (mail === "") { // MAIL
+    	$('input[name="mail"]').addClass('input-error');
+    	$('#labelMail').addClass('input-error');
+    	listeErreur.push('Le champ "Mail" est obligatoire');
+    	ok = false;
+    }
+    
+    if (pseudo === "") { // PSEUDO
+    	$('input[name="pseudo"]').addClass('input-error');
+    	$('#labelPseudo').addClass('input-error');
+    	listeErreur.push('Le champ "Pseudo" est obligatoire');
+    	ok = false;
     }
 	
+    if (password === "") { // PASSWORD
+    	$('input[name="password"]').addClass('input-error');
+    	$('#labelPassword').addClass('input-error');
+    	listeErreur.push('Le champ "Mot de passe" est obligatoire');
+    	ok = false;
+    }
+    
+    if (confirmPassword === "") { // PASSWORD
+    	$('input[name="confirmPassword"]').addClass('input-error');
+    	$('#labelConfirmPassWord').addClass('input-error');
+    	listeErreur.push('Le champ "Confirmer mot de passe" est obligatoire');
+    	ok = false;
+    }
+    
+    if (nom === "") { // MAIL
+    	$('input[name="nom"]').addClass('input-error');
+    	$('#labelNom').addClass('input-error');
+    	listeErreur.push('Le champ "Nom" est obligatoire');
+    	ok = false;
+    }
 	
 	// Contrôle du format de l'adresse mail
+    if (!checkEmail(mail)) {
+    	$('input[name="mail"]').addClass('input-error');
+    	$('#labelMail').addClass('input-error');
+    	listeErreur.push('Le format de l\'adresse mail est invalide');
+    	ok = false;
+    }
 	
 	// Contrôle de l'unicité de l'adresse mail et du pseudo
 	
@@ -24,4 +60,10 @@ function validateInscription() {
 	// Contrôle du format du code postal
     
     return false;
+}
+
+/* Teste le format d'une adresse mail */
+function checkEmail(aTester) {
+    var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return pattern.test(aTester);
 }
