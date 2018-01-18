61d4aeb4dead41c6e7d9441d3d66b84458b6280e
Zegui81 <zeghmati.clement@hotmail.fr>
2018-01-18 13:58:10 +0100

    Séparation page inscription + Modification taille des images

--- src/inscription.php
+++ src/inscription.php
@@ -16,33 +16,9 @@
 		    include 'vue/divers.vue.php';
 		    
 		    displayMenu(false, false);
-		   		
+		   	require 'ajax/inscription.html';
 		?>
-		<div class="white">
-			<div class="produit">
-				<h1 class="page-produit no-merge">Créez un compte</h1>
-				<form class="liste-item-inscription">
-					<div class="item-gauche">
-						<span>Adresse mail :</span><input type=text placeholder="adresse mail" class="input-inscription"><br/>
-						<span>Pseudo :</span><input type="text" placeholder="pseudonyme" class="input-inscription"><br/>
-						<span>Mot de passe :</span><input type="password" placeholder="mot de passe" class="input-inscription"><br/>
-						<span>Confirmer mot de passe :</span><input type="password" placeholder="mot de passe" class="input-inscription"><br/>
-					</div>
-					<div class="item-droit">
-						<span>Nom :</span><input type=text placeholder="nom" class="input-inscription"><br/>
-						<span>Prénom :</span><input type=text placeholder="prénom" class="input-inscription"><br/>
-						<span>Date naissance :</span><input type=date placeholder="date de naissance" class="input-inscription"><br/>
-						<span>Adresse :</span><input type=text placeholder="adresse" class="input-inscription"><br/>
-						<span>Code Postal :</span><input type=text placeholder="code postal" class="input-inscription"><br/>
-						<span>Ville :</span><input type=text placeholder="ville" class="input-inscription"><br/>
-						
-						<span class="btn-inscription btn-valider">Valider</span>
-					</div>
-				</form>
-			</div>
 		
-			<a href="#header"><span id="scroller"></span></a>
-		</div>
 		<footer></footer>
 	</body>
 </html>
