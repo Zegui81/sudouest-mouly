499804c8a21661a80cf9aae22e751f5aff0c83a7
Zegui81 <zeghmati.clement@hotmail.fr>
2018-01-23 12:35:34 +0100

    Correction import

--- src/action/doConnexion.php
+++ src/action/doConnexion.php
@@ -1,4 +1,5 @@
 <?php session_start();
+    require '../bd/divers.bd.php';
     require '../bd/utilisateur.bd.php';
     $_SESSION['pseudo'] = $_POST['pseudo'];
     $_SESSION['statut'] = connexion($_POST['pseudo'], $_POST['password']);
