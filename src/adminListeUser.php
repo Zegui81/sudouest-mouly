ae45b71798012d079c00c7efcd285f65c5dc4362
OhMykol <micka.costa27@gmail.com>
2018-01-27 11:52:10 +0100

    Tableau liste des users et produits

---
+++ src/adminListeUser.php
@@ -0,0 +1,19 @@
+<?php session_start();
+include 'vue/divers.vue.php';
+if (!(isset($_SESSION['statut']) && $_SESSION['statut'] == 1)) {
+    // Si un administrateur n'est pas connecté, on n'autorise pas l'accès à la page
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head><?php includeHead(); ?></head>	
+	<body>
+		<?php displayMenu();
+		    
+		   	require 'tabListeUser.html';
+		   	
+		   	displayScroller(); // Scroller pour remonter
+		?>
+	</body>
+</html>
