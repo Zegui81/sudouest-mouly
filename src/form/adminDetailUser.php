4fe6c860fea8f4873530ca93662cf276ae0c949d
Administrateur <Administrateur@314-8.formation-cci12.fr>
2018-01-30 11:38:45 +0100

    Détail user modif form

--- src/form/adminDetailUser.php
+++ src/form/adminDetailUser.php
@@ -2,7 +2,7 @@
 	<div class="produit">
 		<h1 class="page-produit no-merge">Détail de l'utilisateur <?php echo $user->getPseudo() ?></h1>
 		<div class="conteneur-center">
-    		<form class="liste-item-inscription item-admin gestion-user-admin-info" enctype="multipart/form-data" onsubmit="return validateUpdateProduit()" name="adminCategorie" method="POST" action="action/doUpdateUser.php">
+    		<form class="liste-item-inscription item-admin gestion-user-admin-info" enctype="multipart/form-data" onsubmit="return validateUpdateProduit()" name="adminUser" method="POST" action="action/doUpdateUser.php">
                <div class="admin-item-gauche">	
                	   <span class="remove-error lbl-admin-produit lbl-marge-droite-input">Pseudo :</span>
     	           <input id="promo-prod" type="text" name="pseudo" placeholder="pseudonyme" class="input-inscription remove-error" value="<?php echo $user->getPseudo() ?>" disabled><br/>	           
@@ -11,25 +11,26 @@
                </div>
                <div class="admin-item-droit">
     	           <span class="remove-error lbl-admin-produit lbl-marge-droite-input">Nom :</span>
-    	           <input id="nom-prod" type="text" name="nom" placeholder="nom" class="input-inscription remove-error" value="<?php echo $user->getNom() ?>"><br/>
-    	           <span class="remove-error lbl-admin-produit lbl-marge-droite-input">Prénom :</span>
-    	           <input id="prix-prod" type="text" name="prenom" placeholder="prénom" class="input-inscription remove-error" value="<?php echo $user->getPrenom() ?>"><br/>
-               </div>
-    	        
+    	           <input id="nom-user" type="text" name="nom" placeholder="nom" class="input-inscription remove-error" value="<?php echo $user->getNom() ?>"><br/>
+    	           <span class="lbl-admin-produit lbl-marge-droite-input">Prénom :</span>
+    	           <input type="text" name="prenom" placeholder="prénom" class="input-inscription remove-error" value="<?php echo $user->getPrenom() ?>"><br/>
+               </div>    	        
                <div class="admin-item-gauche">
-    	           <span class="remove-error lbl-admin-produit lbl-marge-droite-input">Adresse :</span>
-    	           <input id="nom-prod" type="text" name="adresse" placeholder="adresse" class="input-inscription remove-error" value="<?php echo $user->getAdresse() ?>"><br/>
-    	           <span class="remove-error lbl-admin-produit lbl-marge-droite-input">CP :</span>
-    	           <input id="prix-prod" type="text" name="codePostal" placeholder="code postal" class="input-inscription remove-error" value="<?php echo $user->getCodePostal() ?>"><br/>
-    	           <span class="remove-error lbl-admin-produit lbl-marge-droite-input">Ville :</span>
-    	           <input id="promo-prod" type="text" name="ville" placeholder="ville" class="input-inscription remove-error" value="<?php echo $user->getVille() ?>"><br/>
+    	           <span class="lbl-admin-produit lbl-marge-droite-input">Adresse :</span>
+    	           <input type="text" name="adresse" placeholder="adresse" class="input-inscription remove-error" value="<?php echo $user->getAdresse() ?>"><br/>
+    	           <span class="lbl-admin-produit lbl-marge-droite-input">CP :</span>
+    	           <input type="text" name="codePostal" placeholder="code postal" class="input-inscription remove-error" value="<?php echo $user->getCodePostal() ?>"><br/>
+    	           <span class="lbl-admin-produit lbl-marge-droite-input">Ville :</span>
+    	           <input type="text" name="ville" placeholder="ville" class="input-inscription remove-error" value="<?php echo $user->getVille() ?>"><br/>
+    	           <span class="lbl-admin-produit lbl-marge-droite-input">Naissance:</span>
+    	           <input type="date" name="naissance" placeholder="naissance" class="input-inscription remove-error" value="<?php echo $user->getNaissance() ?>"><br/>
                </div>
                <div class="admin-item-droit">
     	           <span class="remove-error lbl-admin-produit">Statut :</span>
     	           <select>
     				  <option value="0" class="input-inscription remove-error" <?php echo ($user->getRole() == 0 ? 'selected' : '') ?>>Utilisateur</option>
     				  <option value="1" class="input-inscription remove-error" <?php echo ($user->getRole() == 1 ? 'selected' : '') ?>>Administrateur</option>
-    				</select>
+    				</select><br/><br/>
     	           <input class="btn-inscription btn-valider btn-admin" type="submit" value=Enregistrer>
     	           <input onclick="return goToAdminListeUser()" class="btn-inscription btn-supprimer btn-admin" type="button" value="Annuler">
                </div>
